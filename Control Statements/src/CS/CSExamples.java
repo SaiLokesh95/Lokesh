aea7553e7d952e32eae06212ae338c2be3b3a96c
SaiLokesh95 <sailokesheduru@gmail.com>
2022-05-08 11:43:38 +0530

    LOKE

---
+++ Control Statements/src/CS/CSExamples.java
@@ -0,0 +1,17 @@
+package CS;
+
+public class CSExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("[ ");
+		for(int i=1; i<=5; i++)
+		{
+			System.out.print(((i*i)+1)+" ");
+		}
+		System.out.print("]");
+	}
+
+	}
+
+
---
+++ Control Statements/src/CS/break_Label.java
@@ -0,0 +1,10 @@
+package CS;
+
+public class break_Label {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
